a4dec8a7f28d97ecf1d95a2bc1a3201ee44e137a
sid10h <siddharth.harshe93@gmail.com>
2020-05-03 00:56:33 +0530

    updates to deploy to heroku

--- src/app/number-guess/number-guess.component.ts
+++ src/app/number-guess/number-guess.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-number-guess',
@@ -13,17 +14,36 @@ export class NumberGuessComponent implements OnInit {
   generatedNumber: any;
   displayFailMessage = false;
   isPlay = false;
+  displayNumber: any;
   @ViewChildren('first') firstField: QueryList<ElementRef>;
   @ViewChildren('second') secondField: QueryList<ElementRef>;
   @ViewChildren('third') thirdField: QueryList<ElementRef>;
+  @ViewChildren('fourth') fourthField: QueryList<ElementRef>;
+  @ViewChildren('fifth') fifthField: QueryList<ElementRef>;
 
   ngOnInit(): void {
-    this.generatedNumber = Math.floor(100 + Math.random() * 900);
+
+    var arr = [];
+    var r = Math.floor(Math.random() * 10);
+    if(r != 0) {
+      arr.push(r);
+    }
+    while(arr.length < 5) {
+      var r = Math.floor(Math.random() * 10);
+      if(arr.indexOf(r) === -1) {
+        arr.push(r);
+      }  
+    }
+
+    this.generatedNumber = arr;
     const guessNumber = {} as ThreeDigitNumber;
     guessNumber.first = '';
     guessNumber.second = '';
     guessNumber.third = '';
+    guessNumber.fourth = '';
+    guessNumber.fifth = '';
     this.guessedNumbers.push(guessNumber);
+    this.displayNumber = 1*this.generatedNumber[4] + 10*this.generatedNumber[3] + 100*this.generatedNumber[2] + 1000*this.generatedNumber[1] + 10000*this.generatedNumber[0];
   }
 
   numberOnly(event, index, position): boolean {
@@ -36,8 +56,17 @@ export class NumberGuessComponent implements OnInit {
         element.nativeElement.focus()
       )
     }
+    if (position === 3) {
+      this.fourthField.forEach((element, index) =>
+        element.nativeElement.focus()
+      )
+    }
+    if (position === 4) {
+      this.fifthField.forEach((element, index) =>
+        element.nativeElement.focus()
+      )
+    }
     return true;
-
   }
 
   startPlaying() {
@@ -57,40 +86,56 @@ export class NumberGuessComponent implements OnInit {
   }
 
   testNumber(number, index) {
-     if (number.first === '' || number.second === '' || number.third === '') {
+    if (number.first === '' || number.second === '' || number.third === '') {
       alert('Fill all entries')
       return;
     }
     number.disable = true;
-    const firstNum = parseInt((this.generatedNumber + '')[0]);
-    const secondNum = parseInt((this.generatedNumber + '')[1]);
-    const thirdNum = parseInt((this.generatedNumber + '')[2]);
+    const firstNum = this.generatedNumber[0];
+    const secondNum = this.generatedNumber[1];
+    const thirdNum = this.generatedNumber[2];
+    const fourthNum = this.generatedNumber[3];
+    const fifthNum = this.generatedNumber[4];
     let fixed = 0;
     let variable = 0;
     if (number.first === firstNum) {
       fixed++
-    } else if (number.first === secondNum || number.first === thirdNum) {
+    } else if (number.first === secondNum || number.first === thirdNum || number.first === fourthNum || number.first === fifthNum) {
       variable++
     }
 
     if (number.second === secondNum) {
       fixed++
-    } else if (number.second === firstNum || number.second === thirdNum) {
+    } else if (number.second === firstNum || number.second === thirdNum || number.second === fourthNum || number.second === fifthNum) {
       variable++
     }
 
     if (number.third === thirdNum) {
       fixed++
-    } else if (number.third === secondNum || number.third === firstNum) {
+    } else if (number.third === firstNum || number.third === secondNum || number.second === fourthNum || number.second === fifthNum) {
+      variable++
+    }
+
+    if (number.fourth === fourthNum) {
+      fixed++
+    } else if (number.fourth === firstNum || number.fourth === secondNum || number.fourth === thirdNum || number.fourth === fifthNum) {
+      variable++
+    }
+
+    if (number.fifth === fifthNum) {
+      fixed++
+    } else if (number.fifth === firstNum || number.fifth === secondNum || number.fifth === thirdNum || number.fifth === fourthNum) {
       variable++
     }
     number.displayMessage = true;
     if (this.guessedNumbers.length < 10) {
-      if (fixed != 3) {
+      if (fixed != 5) {
         const guessNumber = {} as ThreeDigitNumber;
         guessNumber.first = '';
         guessNumber.second = '';
         guessNumber.third = '';
+        guessNumber.fourth = '';
+        guessNumber.fifth = '';
         this.guessedNumbers.push(guessNumber);
         this.cursorFirst(this.firstField, index);
         number.message = fixed + ' CNCP and ' + variable + ' CN'
@@ -99,7 +144,7 @@ export class NumberGuessComponent implements OnInit {
         return;
       }
     } else if (this.guessedNumbers.length == 10) {
-      if (fixed != 3) {
+      if (fixed != 5) {
         number.message = fixed + ' CNCP and ' + variable + ' CN'
         this.displayFailMessage = true;
       } else {
@@ -127,6 +172,8 @@ export interface ThreeDigitNumber {
   first: any;
   second: any;
   third: any;
+  fourth: any;
+  fifth: any;
   disable: boolean;
   displayMessage: boolean;
   message: any;
