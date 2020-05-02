9aa81fe72c07380ac90ab36f84ad390b98472d4e
sid10h <siddharth.harshe93@gmail.com>
2020-05-02 18:02:41 +0530

    updates to deploy to heroku

--- src/app/number-guess/number-guess.component.ts
+++ src/app/number-guess/number-guess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-number-guess',
@@ -13,12 +13,12 @@ export class NumberGuessComponent implements OnInit {
   generatedNumber: any;
   displayFailMessage = false;
   isPlay = false;
-  @ViewChild('first') firstField: ElementRef;
-  @ViewChild('second') secondField: ElementRef;
-  @ViewChild('third') thirdField: ElementRef;
+  @ViewChildren('first') firstField: QueryList<ElementRef>;
+  @ViewChildren('second') secondField: QueryList<ElementRef>;
+  @ViewChildren('third') thirdField: QueryList<ElementRef>;
 
   ngOnInit(): void {
-    this.generatedNumber = Math.floor(100 + Math.random()*900);
+    this.generatedNumber = Math.floor(100 + Math.random() * 900);
     const guessNumber = {} as ThreeDigitNumber;
     guessNumber.first = '';
     guessNumber.second = '';
@@ -26,82 +26,104 @@ export class NumberGuessComponent implements OnInit {
     this.guessedNumbers.push(guessNumber);
   }
 
-  numberOnly(event): boolean {
+  numberOnly(event, index, position): boolean {
     const charCode = (event.which) ? event.which : event.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
       return false;
     }
-    //this.thirdField.nativeElement.focus();
+    if (position === 2) {
+      this.thirdField.forEach((element, index) =>
+        element.nativeElement.focus()
+      )
+    }
     return true;
 
   }
 
-  firstNumberOnly(event): boolean {
+  startPlaying() {
+    this.isPlay = !this.isPlay;
+    this.cursorFirst(this.firstField, 0);
+  }
+  firstNumberOnly(event, index): boolean {
     const charCode = (event.which) ? event.which : event.keyCode;
     if (charCode > 31 && (charCode < 49 || charCode > 57)) {
       return false;
     }
-    //this.secondField.nativeElement.focus();
+    this.secondField.forEach((element, index) =>
+      element.nativeElement.focus()
+    );
     return true;
 
   }
 
-  testNumber(number) {
-     if(number.first === '' || number.second === '' || number.third === '') {
+  testNumber(number, index) {
+     if (number.first === '' || number.second === '' || number.third === '') {
       alert('Fill all entries')
       return;
     }
     number.disable = true;
-    const firstNum = parseInt((this.generatedNumber+'')[0]);
-    const secondNum = parseInt((this.generatedNumber+'')[1]);
-    const thirdNum = parseInt((this.generatedNumber+'')[2]);
-    let fixed = 0; 
+    const firstNum = parseInt((this.generatedNumber + '')[0]);
+    const secondNum = parseInt((this.generatedNumber + '')[1]);
+    const thirdNum = parseInt((this.generatedNumber + '')[2]);
+    let fixed = 0;
     let variable = 0;
-    if(number.first === firstNum) { 
+    if (number.first === firstNum) {
       fixed++
     } else if (number.first === secondNum || number.first === thirdNum) {
       variable++
     }
 
-    if(number.second === secondNum) { 
+    if (number.second === secondNum) {
       fixed++
     } else if (number.second === firstNum || number.second === thirdNum) {
       variable++
     }
 
-    if(number.third === thirdNum) { 
+    if (number.third === thirdNum) {
       fixed++
     } else if (number.third === secondNum || number.third === firstNum) {
       variable++
     }
     number.displayMessage = true;
-    if(this.guessedNumbers.length < 10) {
-      if(fixed != 3) {
+    if (this.guessedNumbers.length < 10) {
+      if (fixed != 3) {
         const guessNumber = {} as ThreeDigitNumber;
         guessNumber.first = '';
         guessNumber.second = '';
         guessNumber.third = '';
         this.guessedNumbers.push(guessNumber);
-        number.message = fixed+' CNCP and '+ variable + ' CN'
+        this.cursorFirst(this.firstField, index);
+        number.message = fixed + ' CNCP and ' + variable + ' CN'
       } else {
         number.message = 'All correct!!'
         return;
       }
-    } else if(this.guessedNumbers.length == 10) {
-      if(fixed != 3) {
-        number.message = fixed+' CNCP and '+ variable + ' CN'
+    } else if (this.guessedNumbers.length == 10) {
+      if (fixed != 3) {
+        number.message = fixed + ' CNCP and ' + variable + ' CN'
         this.displayFailMessage = true;
       } else {
         number.message = 'All correct!!'
         return;
       }
     }
-     // this.firstField.nativeElement.focus();
-    }
-
+  }
+  cursorFirst(firstField, index) {
+    setTimeout(function(){
+      firstField.forEach((element, index) =>
+        element.nativeElement.focus()
+      );
+    }, 200)
   }
 
-export interface ThreeDigitNumber { 
+  callPlayAgain() {
+    this.displayFailMessage = false;
+    this.guessedNumbers = []
+    this.ngOnInit();
+  }
+}
+
+export interface ThreeDigitNumber {
   first: any;
   second: any;
   third: any;
