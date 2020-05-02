f465d4874d6407ed19cd78b58705e12b714a2cb3
sid10h <siddharth.harshe93@gmail.com>
2020-05-02 14:44:38 +0530

    first commit

--- src/app/server.js
+++ src/app/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+app.use(express.static('./dist/ng6-proj'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/ng6-proj/' }
+    );
+});
+
 app.listen(process.env.PORT || 8080);
